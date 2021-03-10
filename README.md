2668f40b62e9c36a519d8aef976d96f5f6044db2
dera- <alchemy.and.fairy@gmail.com>
2021-03-10 06:42:05 +0000

    reflect comment

--- README.md
+++ README.md
@@ -1,20 +1,21 @@
 # actions
-akashic-games organization のリポジトリで共通で利用する Github Actions 用のアクションを管理するためのリポジトリ
+akashic-games organization のリポジトリで共通で利用する Github Actions 用のアクションを管理するためのリポジトリ。
+各アクションをディレクトリ単位で切り分けています。
 
 ## 各アクションについて
 ### release
 #### 概要
 指定されたリポジトリに対して以下の処理を行います。
 * npm モジュールの publish　処理
+  * publish時のオプションは、指定されたリポジトリのpackage.jsonの`publishConfig`の内容に準拠します。
 * publish 時の バージョンで Github Release Note を作成
 
 #### 入力パラメータ
 * `github_token`: 対象リポジトリの Github トークン。必須パラメータ
 * `npm_token`: 対象リポジトリの Npm パッケージの Npm トークン。必須パラメータ
 
-### Usage (利用例)
-```
-# relaseアクション利用前にactions/checkout等で対象のgithubリポジトリをcheckoutしておく必要があります
+### 利用例
+```yml
 - name: Checkout repository
   uses: actions/checkout@v2
 - name: Publish and Release
@@ -26,13 +27,20 @@ akashic-games organization のリポジトリで共通で利用する Github Act
     npm_token: ${{ secrets.NPM_TOKEN }}
 ```
 
+### 注意点
+このアクションを使用する場合、対象のパッケージでは以下の対応が必要になります。
+* スコープ化された公開パッケージの場合、package.jsonの`publishConfig.access`にtrueを指定する必要があります。
+
 ## ビルド方法
-以下のコマンドを実行 
+以下のコマンドを実行
 
 ```
 npm install
 npm run build
 ```
+
+上記コマンドによって、各アクションのディレクトリ下に`dist/index.js`というビルド成果物が生成されます。
+アクションを更新するためにはビルド生成物をgit管理する必要があります。
 
 ## テスト方法
 以下のコマンドを実行
--- release/src/index.js
+++ release/src/index.js
@@ -11,13 +11,17 @@ const inputs = {
 const targetDirPath = process.env.GITHUB_WORKSPACE;
 const packageJsonPath = path.join(targetDirPath, "package.json");
 const changelogPath = path.join(targetDirPath, "CHANGELOG.md");
+// GITHUB_REPOSITORYのフォーマットは オーナー名/リポジトリ名 となっているのでそれぞれ抽出する
+const repositoryInfo = process.env.GITHUB_REPOSITORY.split("/");
+const ownerName = repositoryInfo[0];
+const repositoryName = repositoryInfo[1];
+const gitCommitHash = process.env.GITHUB_SHA;
 
 (async () => {
 	try {
 		await npmPublish({
 			package: packageJsonPath,
-			token: inputs.npmToken,
-			access: "public"
+			token: inputs.npmToken
 		});
 		const packageJson = require(packageJsonPath);
 		const version = packageJson["version"];
@@ -27,15 +31,13 @@ const changelogPath = path.join(targetDirPath, "CHANGELOG.md");
 			body = generateReleaseNote(changelog, version);
 		}
 		const octokit = github.getOctokit(inputs.githubToken);
-		// GITHUB_REPOSITORYのフォーマットは オーナー名/リポジトリ名 となっているのでそれぞれ抽出する
-		const repositoryInfo = process.env.GITHUB_REPOSITORY.split("/");
 		await octokit.repos.createRelease({
-			owner: repositoryInfo[0],
-			repo: repositoryInfo[1],
+			owner: ownerName,
+			repo: repositoryName,
 			tag_name: "v" + version,
 			name: "Release " + version,
 			body: body,
-			target_commitish: process.env.GITHUB_SHA
+			target_commitish: gitCommitHash
 		});
 	} catch(error) {
 		core.setFailed(error.message);
