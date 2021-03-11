e2581d29dcdd304b89fbccb8cef5a50c64da44fc
dera- <alchemy.and.fairy@gmail.com>
2021-03-11 06:34:41 +0000

    reflect comment

--- release/src/__test__/generateReleaseNote.test.js
+++ release/src/__test__/generateReleaseNote.test.js
@@ -1,8 +1,7 @@
 const assert = require("assert");
 const generateReleaseNote = require("../generateReleaseNote");
 
-try {
-	const changelog = `
+const changelog = `
 # CHANGELOG
 
 ## 2.0.1
@@ -16,19 +15,21 @@ try {
 
 ## 1.5.1
 `;
-	// CHANGELOGに記載されているバージョンの内容を取得できる
-	assert.strictEqual(generateReleaseNote(changelog, "2.0.1"), "* test1\n\n");
-	assert.strictEqual(generateReleaseNote(changelog, "2.0.0-beta.12"), "* test2\n\n");
-	// CHANGELOGに記載されていないバージョンを指定した場合空文字が返ってくる
-	assert.strictEqual(generateReleaseNote(changelog, "1.5.0"), "");
 
-	// CHANGELOGに記載されているバージョンが1つだけでも上記と同様の挙動となる
-	const changelog2 = `
+const changelog2 = `
 # CHANGELOG
 
 ## 1.0.0
 * first release
 `;
+
+try {
+	// CHANGELOGに記載されているバージョンの内容を取得できる
+	assert.strictEqual(generateReleaseNote(changelog, "2.0.1"), "* test1\n\n");
+	assert.strictEqual(generateReleaseNote(changelog, "2.0.0-beta.12"), "* test2\n\n");
+	// CHANGELOGに記載されていないバージョンを指定した場合空文字が返ってくる
+	assert.strictEqual(generateReleaseNote(changelog, "1.5.0"), "");
+	// CHANGELOGに記載されているバージョンが1つだけでも上記と同様の挙動となる
 	assert.strictEqual(generateReleaseNote(changelog2, "1.0.0"), "* first release\n\n");
 	assert.strictEqual(generateReleaseNote(changelog2, "1.0.1"), "");
 } catch(e) {
